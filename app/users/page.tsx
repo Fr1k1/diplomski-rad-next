2c5cd0d9e663d4ed46034017c69cba0f04c5439e
Martin Friščić <martin.friscic6@gmail.com>
2025-04-27 12:39:38 +0200

    Add shadcn components

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,7 +2,7 @@ import { createClient } from "@/utils/supabase/server";
 
 export default async function Users() {
   const supabase = createClient();
-  const { data: users } = await supabase.from("user").select();
+  const { data: users } = await supabase.from("users").select();
 
   return <pre>{JSON.stringify(users, null, 2)}</pre>;
 }
